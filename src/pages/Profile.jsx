6e3ff5875d960602abe1e99fae253dd3fae16696
Prabhat Mani Srivastava <prabhatmani933@gmail.com>
2023-02-02 02:38:43 +0530

    Created the spinner and new listing interface.

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,9 +1,10 @@
 import React, { useState } from "react";
 import { getAuth, updateProfile } from "firebase/auth";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
 import { doc, updateDoc } from "firebase/firestore";
 import { db } from "./../firebase";
+import { FcHome } from "react-icons/fc";
 
 const Profile = () => {
   const auth = getAuth();
@@ -92,6 +93,18 @@ const Profile = () => {
               </p>
             </div>
           </form>
+          <button
+            type="submit"
+            className="w-full bg-blue-600 text-white uppercase px-7 py-3 text-sm font-medium rounded shadow-md hover:bg-blue-700 transition duration-150 ease-in-out hover:shadow-lg active:bg-blue-800"
+          >
+            <Link
+              to="/create-listing"
+              className="flex justify-center items-center"
+            >
+              <FcHome className="mr-3 text-3xl bg-red-200 rounded-full p-1 border-2" />
+              Sell or rent your Home
+            </Link>
+          </button>
         </div>
       </section>
     </>
