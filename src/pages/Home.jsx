c370e3acb6201fd5e798225244abb930d7352363
Prabhat Mani Srivastava <prabhatmani933@gmail.com>
2023-02-06 07:31:15 +0530

    Added CreateListing button.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -104,77 +104,94 @@ const Home = () => {
   console.log(saleListings);
 
   return (
-    <div>
+    <>
       <Slider />
-      <div className="max-w-6xl mx-auto pt-4 space-y-6">
-        {/* Offer listings */}
-        {offerListings && offerListings.length > 0 && (
-          <div className="m-2 mb-6">
-            <h2 className="px-3 text-2xl mt-6 font-semibold">Recent offers</h2>
-            <Link to="/offers">
-              <p className="px-3 text-sm text-blue-600 hover:text-blue-800 transition duration-150 ease-in-out">
-                Show more offers
-              </p>
-            </Link>
-            <ul className="sm:grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 ">
-              {offerListings.map((listing) => (
-                <ListingItem
-                  key={listing.id}
-                  listing={listing.data}
-                  id={listing.id}
-                />
-              ))}
-            </ul>
-          </div>
-        )}
+      <div className="flex">
+        <div className="max-w-6xl mx-auto pt-4 space-y-6">
+          <Link to="/create-listing">
+            <div className="bg-blue-500 max-w-xs mx-auto rounded-md items-center">
+              <button
+                type="button"
+                className="w-full bg-green-500 text-white px-7 py-3 text-sm font-medium uppercase rounded shadow-md hover:bg-green-700 transition duration-150 ease-in-out hover:shadow-lg active:bg-green-800"
+              >
+                (+) Create a Listing
+              </button>
+            </div>
+          </Link>
 
-        {/* Rent listings */}
-        {rentListings && rentListings.length > 0 && (
-          <div className="m-2 mb-6">
-            <h2 className="px-3 text-2xl mt-6 font-semibold">
-              Places for rent
-            </h2>
-            <Link to="/category/rent">
-              <p className="px-3 text-sm text-blue-600 hover:text-blue-800 transition duration-150 ease-in-out">
-                Show more places for rent
-              </p>
-            </Link>
-            <ul className="sm:grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 ">
-              {rentListings.map((listing) => (
-                <ListingItem
-                  key={listing.id}
-                  listing={listing.data}
-                  id={listing.id}
-                />
-              ))}
-            </ul>
-          </div>
-        )}
+          <div>
+            {/* Offer listings */}
+            {offerListings && offerListings.length > 0 && (
+              <div className="m-2 mb-6">
+                <h2 className="px-3 text-2xl mt-6 font-semibold">
+                  Recent offers
+                </h2>
+                <Link to="/offers">
+                  <p className="px-3 text-sm text-blue-600 hover:text-blue-800 transition duration-150 ease-in-out">
+                    Show more offers
+                  </p>
+                </Link>
+                <ul className="sm:grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 ">
+                  {offerListings.map((listing) => (
+                    <ListingItem
+                      key={listing.id}
+                      listing={listing.data}
+                      id={listing.id}
+                    />
+                  ))}
+                </ul>
+              </div>
+            )}
 
-        {/* Sale listings */}
-        {saleListings && saleListings.length > 0 && (
-          <div className="m-2 mb-6">
-            <h2 className="px-3 text-2xl mt-6 font-semibold">
-              Places for sale
-            </h2>
-            <Link to="/category/sale">
-              <p className="px-3 text-sm text-blue-600 hover:text-blue-800 transition duration-150 ease-in-out">
-                Show more places for sale
-              </p>
-            </Link>
-            <ul className="sm:grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 ">
-              {saleListings.map((listing) => (
-                <ListingItem
-                  key={listing.id}
-                  listing={listing.data}
-                  id={listing.id}
-                />
-              ))}
-            </ul>
+            {/* Rent listings */}
+            {rentListings && rentListings.length > 0 && (
+              <div className="m-2 mb-6">
+                <h2 className="px-3 text-2xl mt-6 font-semibold">
+                  Places for rent
+                </h2>
+                <Link to="/category/rent">
+                  <p className="px-3 text-sm text-blue-600 hover:text-blue-800 transition duration-150 ease-in-out">
+                    Show more places for rent
+                  </p>
+                </Link>
+                <ul className="sm:grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 ">
+                  {rentListings.map((listing) => (
+                    <ListingItem
+                      key={listing.id}
+                      listing={listing.data}
+                      id={listing.id}
+                    />
+                  ))}
+                </ul>
+              </div>
+            )}
+
+            {/* Sale listings */}
+            {saleListings && saleListings.length > 0 && (
+              <div className="m-2 mb-6">
+                <h2 className="px-3 text-2xl mt-6 font-semibold">
+                  Places for sale
+                </h2>
+                <Link to="/category/sale">
+                  <p className="px-3 text-sm text-blue-600 hover:text-blue-800 transition duration-150 ease-in-out">
+                    Show more places for sale
+                  </p>
+                </Link>
+                <ul className="sm:grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 ">
+                  {saleListings.map((listing) => (
+                    <ListingItem
+                      key={listing.id}
+                      listing={listing.data}
+                      id={listing.id}
+                    />
+                  ))}
+                </ul>
+              </div>
+            )}
           </div>
-        )}
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
